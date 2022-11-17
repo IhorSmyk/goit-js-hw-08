8fec221a1a5e263234fd8cddb6840922bc95df9f
Ihor Smyk <ismyk256@gmail.com>
2022-11-18 01:16:09 +0200

    updates applied

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+    return items.map((el) => 
+    `<a class="gallery__item"
+     href="${el.original}">
+      <img class="gallery__image"
+      src="${el.preview}"
+      alt="${el.description}" />
+    </a>`).join('');
+}
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+
+console.log(galleryItems);
