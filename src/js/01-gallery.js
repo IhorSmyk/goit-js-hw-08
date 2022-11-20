31b44946e6216230bb7db1b88bbe30bd7ebfe244
Ihor Smyk <ismyk256@gmail.com>
2022-11-20 22:03:47 +0200

    ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,30 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryRef = document.querySelector(".gallery");
+const galleryRef = document.querySelector('.gallery');
 
 function createGalleryMarkup(items) {
-    return items.map((el) => 
-    `<a class="gallery__item"
+  return items
+    .map(
+      el =>
+        `<a class="gallery__item"
      href="${el.original}">
       <img class="gallery__image"
       src="${el.preview}"
       alt="${el.description}" />
-    </a>`).join('');
+    </a>`
+    )
+    .join('');
 }
 
 galleryRef.innerHTML = createGalleryMarkup(galleryItems);
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import { throttle } from 'lodash';
 
+const KEY_STORAGE = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onTextInput, 500));
+populateText();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const finalData = {};
+  for (const [key, value] of formData.entries()) {
+    if (value === '') {
+      alert('не всі поля заповнені');
+      return;
+    }
+    finalData[key] = value;
+  }
+  console.log(finalData);
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY_STORAGE);
+}
+
+function onTextInput(e) {
+  const { name, value } = e.target;
+  const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (parsedData) {
+    const formData = {
+      ...parsedData,
+      [name]: value,
+    };
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+  } else {
+    const formData = { [name]: value };
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+  }
+}
+
+function populateText() {
+  const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (parsedData) {
+    const inputNames = Object.keys(parsedData);
+    inputNames.forEach(inputName => {
+      const input = formRef.elements[inputName];
+      input.value = parsedData[inputName];
+    });
+  }
+}
